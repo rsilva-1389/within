3239780fa5cd211fbb52486d2061c0ef8d943f00
Renan Farias Silva <rsilva.1389@gmail.com>
2026-06-26 09:36:03 -0300

    Fixing build dir being versioned

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_within_app_WithinApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.within.app.WithinApplication"
-)
-public final class _com_within_app_WithinApplication {
-}
